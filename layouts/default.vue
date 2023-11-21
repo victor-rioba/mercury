7d5a7ea071f2c7555501fe26d75258d384c96460
Sirkastik <riobavictor2019@gmail.com>
2023-11-21 14:12:17 +0300

    feat: add mobile layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="flex w-screen h-screen">
+  <div class="hidden md:flex w-screen h-screen">
     <PageNavbar />
     <UCard class="p-4 w-full h-screen overflow-y-auto">
       <PageBreadcrumbs v-if="useRoute().name !== 'index'" />
       <slot />
     </UCard>
   </div>
+  <UCard class="p-4 w-screen h-screen pb-24 overflow-y-auto md:hidden">
+    <PageBreadcrumbs v-if="useRoute().name !== 'index'" />
+    <slot />
+    <PageNavbarMobile />
+  </UCard>
 </template>
