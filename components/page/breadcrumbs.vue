3175d52fcaa899af19f63728bc2d94d50546bac7
Sirkastik <riobavictor2019@gmail.com>
2023-11-22 21:06:11 +0300

    feat: get breadcrumbs from url

--- components/page/breadcrumbs.vue
+++ components/page/breadcrumbs.vue
@@ -1,12 +1,19 @@
+<script setup lang="ts">
+const fragments = computed(() => {
+  return (useRoute().name as string).split("-").filter((n) => n !== "index");
+});
+</script>
+
 <template>
   <nav
     class="flex px-4 py-2 text-gray-700 border border-gray-200 rounded-lg w-fit mb-8"
     aria-label="Breadcrumb"
+    v-if="fragments.length"
   >
     <ol class="inline-flex items-center space-x-1 md:space-x-3">
       <li class="inline-flex items-center">
-        <a
-          href="#"
+        <NuxtLink
+          to="/"
           class="inline-flex items-center text-xs font-medium text-gray-700 hover:text-green-600"
         >
           <svg
@@ -21,9 +28,9 @@
             ></path>
           </svg>
           Home
-        </a>
+        </NuxtLink>
       </li>
-      <li>
+      <li v-for="(fragment, i) in fragments" :key="fragment">
         <div class="flex items-center">
           <svg
             aria-hidden="true"
@@ -38,31 +45,12 @@
               clip-rule="evenodd"
             ></path>
           </svg>
-          <a
-            href="#"
-            class="ml-1 text-xs font-medium text-gray-700 hover:text-green-600 md:ml-2"
-            >Products</a
+          <NuxtLink
+            :to="`/${fragments.slice(0, i + 1).join('/')}`"
+            class="ml-1 text-xs font-medium text-gray-700 hover:text-green-600 md:ml-2 capitalize"
           >
-        </div>
-      </li>
-      <li aria-current="page">
-        <div class="flex items-center">
-          <svg
-            aria-hidden="true"
-            class="w-6 h-6 text-gray-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-          <span class="ml-1 text-xs font-medium text-gray-500 md:ml-2"
-            >List</span
-          >
+            {{ fragment }}
+          </NuxtLink>
         </div>
       </li>
     </ol>
