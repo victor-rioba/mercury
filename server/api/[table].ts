4bb65ef1bbeb1f085a58e4ded95240b7884b2964
Sirkastik <riobavictor2019@gmail.com>
2023-11-23 12:16:15 +0300

    feat: camelCase tableName in api response

--- server/api/[table].ts
+++ server/api/[table].ts
@@ -1,7 +1,9 @@
+import _ from "lodash";
+
 import type { Table } from "../utils/db";
 
 export default defineEventHandler(async (event) => {
   const table = getRouterParam(event, "table") as Table;
   const data = await useDb(table).select();
-  return { [table]: data };
+  return { [_.camelCase(table)]: data };
 });
