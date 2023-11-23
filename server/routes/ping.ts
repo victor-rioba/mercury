0b5902d60530fea829c523b938de690cc2868b67
Sirkastik <riobavictor2019@gmail.com>
2023-11-23 22:27:17 +0300

    feat: use users table for /ping

--- server/routes/ping.ts
+++ server/routes/ping.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   return {
     status: "up",
-    db: Boolean(await useDb("roles")),
+    db: Boolean(await useDb("users").first()),
   };
 });
