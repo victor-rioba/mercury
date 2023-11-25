d06d74c27cc35120ca2c83273382be0847489090
Sirkastik <riobavictor2019@gmail.com>
2023-11-25 21:32:12 +0300

    feat: add kinde auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui", "@nuxt/image"],
+  modules: ["@nuxt/ui", "@nuxt/image", "@nuxtjs/kinde"],
   colorMode: { preference: "light" },
   ui: { icons: "all" },
   devtools: { enabled: true },
