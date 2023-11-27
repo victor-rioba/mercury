d8bea87af3e3d6a916e8dfcbe0ce15ee667cb2a4
Sirkastik <riobavictor2019@gmail.com>
2023-11-27 23:57:41 +0300

    feat: fetch user by email before serving response

--- server/api/[table].ts
+++ server/api/[table].ts
@@ -3,7 +3,18 @@ import _ from "lodash";
 import type { Table } from "../utils/db";
 
 export default defineEventHandler(async (event) => {
+  let kindeUser;
+  try {
+    kindeUser = await event.context.kinde.getUserProfile();
+  } catch (error) {
+    console.log("Error :: ", (error as any)?.message);
+    return createError({ statusCode: 401 });
+  }
+  let user = await useDb("users")
+    .select("id")
+    .first()
+    .where("email", kindeUser.email);
   const table = getRouterParam(event, "table") as Table;
   const data = await useDb(table).select();
-  return { [_.camelCase(table)]: data };
+  return { user, [_.camelCase(table)]: data };
 });
